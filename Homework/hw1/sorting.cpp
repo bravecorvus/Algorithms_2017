65e609ae7693c493f34db0b86045ffeb84b3876d
Andrew Lee <leeas@stolaf.edu>
2017-02-07 19:51:34 -0600

    part C done

--- Homework/hw1/sorting.cpp
+++ Homework/hw1/sorting.cpp
@@ -1,10 +1,40 @@
+#include <chrono>
+#include <typeinfo>
 #include <iostream>
-#include <array>
 using namespace std;
+using namespace chrono;
 
 int main() {
-    std::array<int, 4> a1{{0, 1, 2, 3}};
-    int i = 0;
-    for (i = 0; i < sizeof(a1)/sizeof(int); ++i){} //emtpy loop to save length of loop into i
-    cout << i
+    int initiallist[4] = {0, 1, 2, 3};
+    unsigned long starttime = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    int finallist[sizeof(initiallist)/sizeof(int)];
+    for (int i = 0; i < sizeof(initiallist)/sizeof(int); ++i) {
+        if (i == 0) {
+            finallist[0] = initiallist[0];
+        } else {
+            if (finallist[0] < initiallist[i]) {
+                finallist[0] = initiallist[i];
+            }
+        }
+    }
+    int lastlargest = finallist[0];
+    int currentlargest;
+    for (int x = 1; x < sizeof(initiallist)/sizeof(int); ++x) {
+        for (int i = 0; i < sizeof(initiallist)/sizeof(int); ++i) {
+            if (i == 0 && initiallist[0] < lastlargest) {
+                currentlargest = initiallist[0];
+            } else {
+                if (currentlargest < initiallist[i] && initiallist[i] < lastlargest) {
+                    currentlargest = initiallist[i];
+                }
+            }
+        }
+        finallist[x] = currentlargest;
+        lastlargest = currentlargest;
+    }
+    unsigned long endtime = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    for (int i = 0; i < sizeof(finallist)/sizeof(int); ++ i) {
+        cout << finallist[i] << endl;
+    }
+    cout << "Algorithm took " << endtime-starttime << " nanoseconds to complete" << endl;
 }
