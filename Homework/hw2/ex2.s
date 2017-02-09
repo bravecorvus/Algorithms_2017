bc2a2f5e4af9a51937fa7ccabee350d810d504f0
Andrew Lee <leeas@stolaf.edu>
2017-02-09 17:37:23 -0600

    aa

---
+++ Homework/hw2/ex2.s
@@ -0,0 +1,21 @@
+main:
+.LFB969:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$3, -4(%rbp)
+	movl	$5, -8(%rbp)
+	movl	-4(%rbp), %eax
+	movl	-8(%rbp), %edx
+	movl	%edx, %ecx
+	subl	%eax, %ecx
+	movl	%ecx, %eax
+	movl	%eax, -12(%rbp)
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
