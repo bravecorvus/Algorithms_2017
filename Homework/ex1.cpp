5fb22f9868831c29cc614bda5a2fabccbf10cde4
Andrew Lee <leeas@stolaf.edu>
2017-02-11 15:38:48 -0600

    aa

--- Homework/ex1.cpp
+++ Homework/ex1.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
+
 #include "elapsed_time.h"
 using namespace std;
 
 int main() {
-    start_timer();
+      start_timer();
     int n = 100;
     int x;
     for ( int i = 0; i < n; ++i ) {
         x = 2+3;
     }   
-    double cycles = elapsed_time();  // also from elapsed_time.h
-    cout << cycles << endl;
 }
