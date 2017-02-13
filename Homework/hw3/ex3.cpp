e0f481d880424b7c0c2dcb21066fe20179decd41
Andrew Lee <leeas@stolaf.edu>
2017-02-13 12:12:35 -0600

    a

--- Homework/hw3/ex3.cpp
+++ Homework/hw3/ex3.cpp
@@ -7,17 +7,12 @@ int main() {
     int n = 100;
     int x;
     start_timer();
-int t = 0;
 for ( int i = 0; i < n; ++i ) {
   for ( int j = 0; j < i/2; ++j ) {
     x = 2398 * 2948 + 3498 * 3948 + 39843 * atan2(32,49);
-    t += 1;
-    // cout << "FUCK" << endl;
   }
-  // cout << "\n" << endl;
   i = i + 2;
 }
-    // double cycles = elapsed_time();  // also from elapsed_time.h
-    // cout << cycles << endl;
-    cout << t << endl;
+    double cycles = elapsed_time();  // also from elapsed_time.h
+    cout << cycles << endl;
 }
