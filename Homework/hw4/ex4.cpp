26ec618182cd94b5f3a76020a49482972fd55912
Andrew Lee <leeas@stolaf.edu>
2017-02-16 15:38:13 -0600

    i removed the other code in my hw4 directory

--- Homework/hw4/ex4.cpp
+++ Homework/hw4/ex4.cpp
@@ -4,14 +4,13 @@
 using namespace std;
 
 int main() {
-    int n = 100;
+    int n = 1000;
     int x;
-    // int t = 0;
     start_timer();
     for ( int i = 0; i < n; ++i ) {
-        for ( int j = 0; j < n/2; ++j ) {
-            x = sin(.32);
-        }
+      for ( int j = i; j < n; ++j ) {
+        x = 38 + 29*(348*tan(.328));
+      }
     }
     double cycles = elapsed_time();  // also from elapsed_time.h
     cout << cycles << endl;
