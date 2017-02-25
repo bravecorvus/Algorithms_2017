26e011f5834092c8f9f49c5a77328cbc997bb86b
Andrew Lee <leeas@stolaf.edu>
2017-02-25 17:46:19 -0600

    test

--- Homework/hw9/checkspell2.cpp
+++ Homework/hw9/checkspell2.cpp
@@ -1,34 +1,17 @@
-#include <typeinfo>
 #include<iostream>
 #include<fstream>
 #include<vector>
 using namespace std;
 #include"elapsed_time.h"
 
-int hashfunction(string arg) {
-    char b[arg.size()];
-    for(int i = 0; i < arg.size(); ++i) {
-        b[i] = arg[i];
-    }
-    int first = b[0];
-    int last = b[sizeof(b)/sizeof(char)-1];
-    int second;
-    if (sizeof(b)/sizeof(char) == 1) {
-        second = last;
-    } else {
-        second = b[1];
-    }
-    int modthis = 499;
-    return(second * (first+last)%modthis);
-}
-
 struct Dictionary {
   vector<string> wordArray;   // store the data
-  vector<string> *hasheddictionary;
+
+  vector<string>* hash; //
+
   Dictionary(const char *filename);  // constructor
 
   bool inWordArray(string &s);       // single query
-  void getWordsFromDictionary(const char *, vector<string>*);
   void check(const char *filename);  // multiple queries
 };
 
@@ -43,37 +26,51 @@ void getWords( const char *filename, vector<string> &vec )
   while ( f >> s ) vec.push_back(s);
 }
 
-void getWordsFromDictionary( const char *filename,  vector<string> *hasheddictionary)
+int hashfunction(string arg) {
+    char b[arg.size()];
+    for(int i = 0; i < arg.size(); ++i) {
+        b[i] = arg[i];
+    }
+    int first = b[0];
+    int last = b[sizeof(b)/sizeof(char)-1];
+    int second;
+
+    if (sizeof(b)/sizeof(char) == 1) {
+        second = last;
+    } else {
+        second = b[1];
+    }
+
+    int modthis = 499;
+
+    return int(second * (first+last)%modthis);
+}
+
+Dictionary::Dictionary( const char *filename )
 {
+  hash=new vector<string> [499];
+
   ifstream f(filename);
   if ( ! f.good() ) {
     cerr << "Error:  unable to open " << filename << endl;
     exit(-1);
   }
   string s;
-  // while ( f >> s ) gethash()
-  while ( f >> s ) {
-    hasheddictionary[hashfunction(s)].push_back(s);
-    // cout << typeid((hasheddictionary[hashfunction(s)])).name() << endl;
+  
+  while ( f >> s ) hash[hashfunction(s)].push_back(s);//make a hash
+
+  //getWords(filename, wordArray);
+}
+
+bool Dictionary::inWordArray(string &s){
+    int index=hashfunction(s);
+    for(int n=0; n<sizeof(hash[index]); n++){
+      if(s==hash[index][n]){
+        return true;
+      }
+    }
+    return false;
   }
-}
-
-Dictionary::Dictionary( const char *filename )
-{
-  hasheddictionary = new vector <string> (500);
-  getWordsFromDictionary(filename, hasheddictionary);
-}
-
-bool Dictionary::inWordArray(string &s)
-{
-  int index = hashfunction(s);
-  int n = hasheddictionary[index].size();
-  for (int i = 0; i < n; ++i)
-    if ( s == hasheddictionary[index][i] ) return true;
-  return false;
-}
-
-
 
 void Dictionary::check( const char *filename )
 {
@@ -83,7 +80,7 @@ void Dictionary::check( const char *filename )
   start_timer();  // from elapsed_time.h
 
   int counter = 0, n = query.size();
-  for ( int i = 0; i < n; ++i ) {
+  for ( int i = 0; i < n; ++i ) { 
     if ( ! inWordArray(query[i]) ) {
       // cout << query[i] << " ";
       ++counter;
